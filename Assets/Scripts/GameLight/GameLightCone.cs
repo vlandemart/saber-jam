6f3714f59f930f0b97d246c414027a98ebd16ad6
vlandemart <vlandemart@gmail.com>
2021-09-25 17:54:12 +0300

    smoke, stuff

--- Assets/Scripts/GameLight/GameLightCone.cs
+++ Assets/Scripts/GameLight/GameLightCone.cs
@@ -3,19 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IGameLightConeTarget {
+public interface IGameLightConeTarget
+{
   void OnHighlightStart();
   void OnHighlightEnd();
 }
 
-public class GameLightCone : MonoBehaviour {
-
-  private void OnTriggerEnter(Collider other) {
+public class GameLightCone : MonoBehaviour
+{
+  private void OnTriggerEnter(Collider other)
+  {
     // Debug.Log("kek trigger enter");
     other.GetComponent<IGameLightConeTarget>()?.OnHighlightStart();
   }
 
-  private void OnTriggerExit(Collider other) {
+  private void OnTriggerExit(Collider other)
+  {
     // Debug.Log("kek trigger exit");
     other.GetComponent<IGameLightConeTarget>()?.OnHighlightEnd();
   }
--- Assets/Scripts/GameLight/TestGameLightMover.cs
+++ Assets/Scripts/GameLight/TestGameLightMover.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestGameLightMover : MonoBehaviour {
+public class TestGameLightMover : MonoBehaviour
+{
   private Vector3 initPos;
-  // Start is called before the first frame update
-  void Start() { initPos = transform.position; }
 
-  // Update is called once per frame
-  void Update() {
-    transform.position =
-        new Vector3(initPos.x, initPos.y,
-                    initPos.z + 2 * (Mathf.Sin(Time.time * 0.3f) + 1));
+  void Start()
+  {
+      initPos = transform.position;
+  }
+  
+  void Update()
+  {
+    transform.position = new Vector3(initPos.x, initPos.y, initPos.z + 2 * (Mathf.Sin(Time.time * 0.3f) + 1));
   }
 }
