5ecb72859e15cfe5ada75c5185993f61274f3e46
Kildyushkin_Dmitriy <kild-dima@yandex.ru>
2021-09-25 23:35:48 +0300

    player rotate

---
+++ Assets/Scripts/PlayerRotationToMouse.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotationToMouse : MonoBehaviour
+{
+    [SerializeField] private float rotationStepDegrees = 30.0f;
+    [SerializeField] private float offsetDegrees = 45.0f;
+
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        var playerPos = Camera.main.WorldToScreenPoint(transform.position);
+        var vec = mousePos - playerPos;
+        float angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg;
+
+        if (angle < 0)
+        {
+            angle += 360.0f;
+        }
+
+        int angleFullParts = (int)(angle / rotationStepDegrees);
+        float angleRemainder = angle % rotationStepDegrees;
+
+        if (angleRemainder > rotationStepDegrees * 0.5f)
+        {
+            angleFullParts++;
+        }
+
+        float angleSnapped = rotationStepDegrees * angleFullParts;
+
+        // Debug.Log("angle = " + angle + " snapped = " + angleSnapped);
+
+        transform.rotation = Quaternion.Euler(0, -angleSnapped - offsetDegrees, 0);
+    }
+}
