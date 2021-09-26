f5ccf42933c36a2eb23e7a5ee542c5b0319044d3
Kildyushkin_Dmitriy <kild-dima@yandex.ru>
2021-09-26 15:10:57 +0300

    stun

--- Assets/Scripts/PersonalMovement.cs
+++ Assets/Scripts/PersonalMovement.cs
@@ -1,18 +1,24 @@
+using System;
 using UnityEngine;
 
 public class PersonalMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public Animator Animator;
-    
+
     public float speed = 6f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (this.IsStunned())
+        {
+            return;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        
+
         Vector3 forward = Camera.main.transform.forward;
         Vector3 right = Camera.main.transform.right;
 
@@ -20,7 +26,7 @@ public class PersonalMovement : MonoBehaviour
         right.y = 0f;
         forward.Normalize();
         right.Normalize();
-        
+
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= 0.1f)
         {
@@ -29,7 +35,7 @@ public class PersonalMovement : MonoBehaviour
             movementVector *= speed * Time.fixedDeltaTime;
             movementVector.y = rb.velocity.y;
             rb.velocity = movementVector;
-            
+
             Animator.SetFloat("velocity", movementVector.magnitude);
         }
         else
