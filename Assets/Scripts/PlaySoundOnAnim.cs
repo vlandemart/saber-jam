d4613df897fa61c53d65f0d1457def3a752ac462
vlandemart <vlandemart@gmail.com>
2021-09-26 20:26:12 +0300

    more sounds

---
+++ Assets/Scripts/PlaySoundOnAnim.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlaySoundOnAnim : MonoBehaviour
+{
+    [SerializeField] private AudioSource sound;
+
+    public void PlaySound()
+    {
+        sound.pitch = Random.Range(.9f, 1.1f);
+        sound.Play();
+    }
+}
