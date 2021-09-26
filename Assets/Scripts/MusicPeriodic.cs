7c6f9143d2507d936625619575f4cb7e462449d3
vlandemart <vlandemart@gmail.com>
2021-09-26 20:56:21 +0300

    scary sounds

---
+++ Assets/Scripts/MusicPeriodic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPeriodic : MonoBehaviour
+{
+    [SerializeField] private AudioSource sound;
+    [SerializeField] private float playPeriod;
+
+    private void Start()
+    {
+        StartCoroutine(PlaySound());
+    }
+
+    private IEnumerator PlaySound()
+    {
+        while (true)
+        {
+            sound.pitch = Random.Range(.9f, 1.1f);
+            sound.Play();
+            yield return new WaitForSeconds(playPeriod);
+        }
+    }
+}
